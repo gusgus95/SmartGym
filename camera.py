e1eac31c419a9e2a055597d923f894044cecac71
gusgus95 <cjsgk9595@naver.com>
2021-11-01 17:39:46 +0900

    수정본 1.0.1

--- camera.py
+++ camera.py
@@ -23,8 +23,8 @@ def get_bounding_box_of_human(camera_num: int, process_title: str = None, shared
     cv2.destroyAllWindows()
 
     # using gpu
-    yolo_net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-    yolo_net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+    # yolo_net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    # yolo_net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
     with open("yolo.names", "r") as f:
         classes = [line.strip() for line in f.readlines()]
@@ -34,8 +34,8 @@ def get_bounding_box_of_human(camera_num: int, process_title: str = None, shared
     while True:
         ret, frame = cap.read()
         # using gpu
-        gpu_frame = cv2.cuda_GpuMat()
-        gpu_frame.upload(frame)
+        # gpu_frame = cv2.cuda_GpuMat()
+        # gpu_frame.upload(frame)
 
         h, w, c = frame.shape
         blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
